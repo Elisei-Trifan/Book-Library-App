ed1721bbf333330d4039175e6fbbef61183350b6
Elisei-Trifan <slavaberzan@gmail.com>
2024-09-23 17:31:00 +0300

    Preparing application components and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
+import BookForm from './component/BookForm/BookForm'
+import BookList from './component/BookList/BookList'
+import Filter from './component/Filter/Filter'
+
 import './App.css'
 
 function App() {
@@ -6,6 +10,15 @@ function App() {
       <header className="app-header">
         <h1>Book Library App</h1>
       </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          {/* <Filter /> */}
+          <BookList />
+        </div>
+      </main>
     </div>
   )
 }
