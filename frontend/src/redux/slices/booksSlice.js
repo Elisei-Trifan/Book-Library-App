77b790280c99b139b04b7f62fd476b0c0be5ef53
Elisei-Trifan <slavaberzan@gmail.com>
2024-09-27 14:53:44 +0300

    create extraReducers

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -28,18 +28,14 @@ const booksSlice = createSlice({
       )
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBook.fulfilled, (state, action) => {
+      if (action.payload.title && action.payload.author) {
+        state.push(createBookWithId(action.payload, 'API'))
+      }
+    })
+  },
 })
-
-export const thunkFunction = async (dispatch, getState) => {
-  try {
-    const res = await axios.get('http://localhost:4000/random-book')
-    if (res.data && res.data.title && res.data.author) {
-      dispatch(addBook(createBookWithId(res.data, 'API')))
-    }
-  } catch (error) {
-    console.log('Error fetching random book', error)
-  }
-}
 
 export default booksSlice.reducer
 
